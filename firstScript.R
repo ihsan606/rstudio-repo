00c689e7f300eba00431b5ec0157fe29fe3e7c0f
ihsan606 <ihsansyafiul@gmail.com>
2021-03-05 23:02:12 +0700

    latihan matrix

--- firstScript.R
+++ firstScript.R
@@ -1,1 +1,18 @@
-A <- 6
+A <- matrix(data = c(1,2,3,4),nrow = 4,ncol = 1,byrow = TRUE)
+A
+M <- matrix(c(1:9),3,3,TRUE)
+M
+M[1,2]
+M[1,] # mengakses semua baris ke-1
+M[,2]
+M[1:2,]
+diag(M)
+
+B <- matrix(1:9,3,3,TRUE)
+B
+colnames(B) <- c("satu","dua","tiga")
+rownames(B) <- c("satu","dua","tiga")
+B
+B["satu","tiga"]
+B[,"dua"]
+B[c("satu","dua"),]
